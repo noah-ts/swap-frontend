e81bc91e30add0a6591bf52a0258b17034a568fe
Noyan <noyan.alimov@gmail.com>
2022-09-24 22:57:09 +0100

    please help

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,11 +4,10 @@ import { Layout } from '../components/Layout'
 
 const Test = () => {
     const { connection } = useConnection()
-    const { publicKey, sendTransaction, signTransaction } = useWallet()
+    const { publicKey, sendTransaction } = useWallet()
 
     const sendSol = async () => {
         if (!publicKey) return
-        if (!signTransaction) return
 
         const txn = new Transaction()
         txn.add(SystemProgram.transfer({
@@ -17,11 +16,7 @@ const Test = () => {
             lamports: 0.01 * LAMPORTS_PER_SOL
         }))
         try {
-            const { blockhash } = await connection.getLatestBlockhash()
-            txn.recentBlockhash = blockhash
-            txn.feePayer = publicKey
-            const signed = await signTransaction(txn)
-            const signature = await connection.sendRawTransaction(signed.serialize())
+            const signature = await sendTransaction(txn, connection)
             console.log(signature)
         } catch (error) {
             console.error('Error sending 0.01 sol: ', error)
@@ -29,7 +24,7 @@ const Test = () => {
     }
 
     return <Layout>
-        <button className='bg-zinc-900 text-zinc-300 p-4 rounded' onClick={sendSol}>send 0.01 sol</button>
+        <button className='bg-zinc-900 text-zinc-300 p-4 rounded cursor-pointer' onClick={sendSol}>send 0.01 sol</button>
     </Layout>
 }
 

6addfd888b111ae8013501c99cc17e70fe7a1df9
Noyan <noyan.alimov@gmail.com>
2022-09-28 22:08:28 +0100

    program id

--- services/utils.ts
+++ services/utils.ts
@@ -3,8 +3,7 @@ import { Connection, PublicKey } from '@solana/web3.js'
 import idl from '../bsl_swap.json'
 import { BslSwap } from '../types/bsl_swap'
 
-// export const programId = new PublicKey(idl.metadata.address)
-export const programId = new PublicKey('FLjoHCAmjojgt7DUxid7WR9EyEj2Pysq9cMehRiM5vtp')
+export const programId = new PublicKey(idl.metadata.address)
 let program: Program<BslSwap>
 
 export const getAnchorProgram = (connection: Connection, wallet: Wallet) => {
